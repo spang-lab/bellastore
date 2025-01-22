d5626434a8ece942a692f0d14488bae844cccd61
Paul Huettl <paul.huettl@mathematik.uni-regensburg.de>
2025-01-22 16:51:18 +0000

    cleaned init

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,1 @@
-from .database.database import ScanDatabase
-from .database.ingress import IngressTable
-from .database.storage import StorageTable
-from .filesystem.ingress import Ingress
-from .filesystem.storage import Storage
 
-__all__ = [
-    "ScanDatabase",
-    "Ingress",
-    "Storage",
-    "IngressTable"
-    "StorageTable"
-]
